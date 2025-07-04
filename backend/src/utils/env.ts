3e6ef8c9bc1c22589b39af0d42d554d39c9b4f68
Dheeraj Reddy K <dheerajreddy070@gmail.com>
2025-07-04 14:27:43 +0530

    Added mongoDB in backend

--- backend/src/utils/env.ts
+++ backend/src/utils/env.ts
@@ -1,7 +1,14 @@
-export function env(key: string): string {
-  const value = process.env[key];
-  if (typeof value === 'undefined') {
-    throw new Error(`❌ Missing required environment variable: ${key}`);
+import * as dotenv from 'dotenv';
+dotenv.config(); // { path: `.env.${process.env.NODE_ENV}` }
+
+export function env(key: string, defaultValue: null | string = null): string {
+  return process.env[key] ?? (defaultValue as string);
+}
+
+export function envOrFail(key: string): string {
+  if (typeof process.env[key] === 'undefined') {
+    throw new Error(`Environment variable ${key} is not set.`);
   }
-  return value;
+
+  return process.env[key] as string;
 }
